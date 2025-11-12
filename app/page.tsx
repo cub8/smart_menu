8f870d4581faa2f2738f1d8d924dc014a8a8f84b
Dawid Ząbek <120474310+DawidZabek@users.noreply.github.com>
2025-11-13 00:05:17 +0100

    Refactor layout and update link formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Roboto_Mono } from 'next/font/google';
-import Link from 'next/link'
+import Link from 'next/link';
 
 const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
@@ -8,16 +8,20 @@ const roboto_mono = Roboto_Mono({
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white">
+    <div className="min-h-screen flex items-center justify-center">
       <div
         className={`${roboto_mono.className} bg-purple-100 rounded-2xl shadow-[0_0_30px_rgba(0,0,0,0.15)] p-10 max-w-md text-center`}
       >
         <h1 className="text-gray-400 text-xl font-extrabold">
           Smart Menu - tygodniowy planer posiłków
-          <br/>
-          <Link href='/planner' className='text-amber-800 hover:bg-yellow-200 underline'> Kliknij, aby zobaczyć planer. </Link>
-          <br/>
-          <Link href='/meals' className='text-blue-800 hover:bg-blue-200 underline'> Zobacz listę posiłków. </Link>
+          <br />
+          <Link href="/planner" className="text-amber-800 hover:bg-yellow-200 underline">
+            Kliknij, aby zobaczyć planer.
+          </Link>
+          <br />
+          <Link href="/meals" className="text-blue-800 hover:bg-blue-200 underline">
+            Zobacz listę posiłków.
+          </Link>
         </h1>
       </div>
     </div>
