96c297c3abfbf1b06a92a112c4f200d8315deb12
Igor Mariak <i.mariak@outlook.com>
2025-11-16 00:51:15 +0100

    Show only user's meals and update seed with more examples

--- app/components/MealCalendar.tsx
+++ app/components/MealCalendar.tsx
@@ -4,95 +4,42 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import plLocale from "@fullcalendar/core/locales/pl";
-import { Meal } from '../models/meal';
 import MealCardWeek from './MealCardWeek';
 import MealCardMonth from './MealCardMonth';
+import { MealPlan, Meal } from "../generated/prisma/client";
 
 
-// PRZYKLADY
-const sampleMeals: Meal[] = [
-  {
-    id: "1",
-    name: "Owsianka z owocami",
-    date: "2025-11-12",
-    type: "breakfast",
-    ingredients: ["płatki owsiane", "mleko", "maliny"]
-  },
-  {
-    id: "2",
-    name: "Kurczak z wymyślnym sosem",
-    date: "2025-11-06",
-    type: "lunch",
-    ingredients: ["pierś z kurczaka", "ryż"]
-  },
-  {
-    id: "3",
-    name: "Kanapki z łososiem i awokado",
-    date: "2025-11-06",
-    type: "dinner",
-    ingredients: ["chleb", "awokado", "łosoś"]
-  },
-  {
-    id: "5",
-    name: "Tosty z szynka",
-    date: "2025-11-10",
-    type: "dinner",
-    ingredients: ["sałata lodowa", "kurczak"]
-  },
-    {
-    id: "7",
-    name: "Kanapka z serem",
-    date: "2025-11-09",
-    type: "breakfast",
-    ingredients: ["sałata lodowa", "kurczak"]
-  },
-    {
-    id: "8",
-    name: "Kotlet schabowy",
-    date: "2025-11-15",
-    type: "lunch",
-    ingredients: ["sałata lodowa", "kurczak"]
-  },
-    {
-    id: "9",
-    name: "Obiad na miescie",
-    date: "2025-11-20",
-    type: "lunch",
-    ingredients: ["sałata lodowa", "kurczak"]
-  },
-    {
-    id: "11",
-    name: "Obiad na miescie",
-    date: "2025-11-21",
-    type: "lunch",
-    ingredients: ["sałata lodowa", "kurczak"]
-  },
-  
-];
+// potrzebne bo VSCode bardzo krzyczal
+type MealPlanWithMeal = MealPlan & { meal: Meal };
 
-const MealCalendar = () => {
+type MealCalendarProps = {
+  mealPlan: any[]; 
+};
+
+const MealCalendar = ({mealPlan}: MealCalendarProps) => {
+
     // kolejnosc ulozenia w kalendarzu
-    const mealOrder = { breakfast: 1, lunch: 2, dinner: 3 };
+    const mealOrder = { BREAKFAST: 1, LUNCH: 2, DINNER: 3 };
 
     // mapowanie na eventy dla Full Calendar
-    const events = sampleMeals.map((m) => ({
-        id: m.id,
-        title: m.name,
+    const events = mealPlan.map((m) => ({
+        id: String(m.id),
+        title: m.meal.name,
         start: m.date,
         allDay: true,
-        extendedProps: m,
-        order: mealOrder[m.type],
+        extendedProps: m
     }));
 
+
     const eventWeekContent = (arg: EventContentArg) => {
-        const meal: Meal = arg.event.extendedProps as Meal; //arg.event.extendedProps to obiekt Meal
+        const mealPlan = arg.event.extendedProps as MealPlanWithMeal; //arg.event.extendedProps to obiekt Meal
         // trzeba zwrocic
-        return <MealCardWeek meal={meal} />;
+        return <MealCardWeek meal={mealPlan.meal} type={mealPlan.type}/>;
     };
 
     const eventMonthContent = (arg: EventContentArg) => {
-        const meal: Meal = arg.event.extendedProps as Meal; //arg.event.extendedProps to obiekt Meal
-        return <MealCardMonth meal={meal} />;
+        const mealPlan = arg.event.extendedProps as MealPlanWithMeal; //arg.event.extendedProps to obiekt Meal
+        return <MealCardMonth meal={mealPlan.meal} type={mealPlan.type} />;
     };
 
 
