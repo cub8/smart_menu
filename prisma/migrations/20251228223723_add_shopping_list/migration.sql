886f7456c70243f11e885d4a4a424b98bae8f615
Igor Mariak <i.mariak@outlook.com>
2025-12-28 23:38:48 +0100

    Add ShoppingList table

---
+++ prisma/migrations/20251228223723_add_shopping_list/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "ShoppingList" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "items" JSONB NOT NULL,
+
+    CONSTRAINT "ShoppingList_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ShoppingList" ADD CONSTRAINT "ShoppingList_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
